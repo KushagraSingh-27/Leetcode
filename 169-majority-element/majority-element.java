efd0317a3b84ee0e2b2b309302d9de0d99bf1145
KushagraSingh-27 <kkushagra.yadav27@gmail.com>
2026-09-02 01:13:42 +0530

    Time: 2 ms (70.75%) | Memory: 55.8 MB (17.95%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int c = 0;
+        int e = 0;
+        for (int i = 0; i < n; i++) {
+            if (c == 0) {
+                c = 1;
+                e = nums[i];
+            } else if (e == nums[i]) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+        int c1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == e) {
+                c1++;
+            }
+        }
+        if (c1 > (n / 2)) {
+            return e;
+        }
+        return -1;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {2, 2, 1, 1, 1, 2, 2};
+        Solution sol = new Solution();
+        int ans = sol.majorityElement(a);
+        System.out.println(ans);
+    }
+}
